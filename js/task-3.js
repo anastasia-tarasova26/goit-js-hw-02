10c0c5426dfb2d84a90d73c21a9948cbfa9c521c
anastasia-tarasova26 <nastya.tarasova2682@gmail.com>
2023-11-26 19:22:29 +0200

    +

--- js/task-3.js
+++ js/task-3.js
@@ -1,20 +1,26 @@
-const banWord1 = spam;
-const banWord2 = sale;
+const banWord1 = "spam";
+const banWord2 = "sale";
 
-const sentens1 = "Latest technology news"; // false
-const sentens2 = "JavaScript weekly newsletter"; // false
-const sentens3 = "Get best sale offers now!"; // true
-const sentens4 = "Amazing SalE, only tonight!"; // true
-const sentens5 = "Trust me, this is not a spam message"; // true
-const sentens6 = "Get rid of sPaM emails. Our book in on sale!"; // true
-const sentens7 = "[SPAM] How to earn fast money?"; // true
+const message1 = "Latest technology news"; // false
+const message2 = "JavaScript weekly newsletter"; // false
+const message3 = "Get best sale offers now!"; // true
+const message4 = "Amazing SalE, only tonight!"; // true
+const message5 = "Trust me, this is not a spam message"; // true
+const message6 = "Get rid of sPaM emails. Our book in on sale!"; // true
+const message7 = "[SPAM] How to earn fast money?"; // true
 
 function checkForSpam(message) {
-  console.log(checkForSpam("Latest technology news")); // false
-  console.log(checkForSpam("JavaScript weekly newsletter")); // false
-  console.log(checkForSpam("Get best sale offers now!")); // true
-  console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-  console.log(checkForSpam("Trust me, this is not a spam message")); // true
-  console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-  console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+  const hasSpam1 = message.toLowerCase().includes(banWord1);
+  const hasSpam2 = message.toLowerCase().includes(banWord2);
+
+  console.log(hasSpam1);
+  console.log(hasSpam2);
 }
+
+checkForSpam(message1);
+checkForSpam(message2);
+checkForSpam(message3);
+checkForSpam(message4);
+checkForSpam(message5);
+checkForSpam(message6);
+checkForSpam(message7);
--- js/task-4.js
+++ js/task-4.js
@@ -1,3 +1,32 @@
+function getShippingCost(country) {
+  let prise;
+  let result;
+
+  switch (country) {
+    case "Australia":
+      prise = 170;
+      result = `Shipping to ${country} will cost ${prise} credits`;
+      break;
+    case "China":
+      prise = 100;
+      result = `Shipping to ${country} will cost ${prise} credits`;
+      break;
+    case "Chile":
+      prise = 250;
+      result = `Shipping to ${country} will cost ${prise} credits`;
+      break;
+    case "Jamaica":
+      prise = 120;
+      result = `Shipping to ${country} will cost ${prise} credits`;
+      break;
+
+    default:
+      result = `Sorry, there is no delivery to your country`;
+      break;
+  }
+  return result;
+}
+
 console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
 console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
 console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
