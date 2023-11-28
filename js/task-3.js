b0fc44eaf149f6ad1fe5343eb394e7c6a8087e83
anastasia-tarasova26 <nastya.tarasova2682@gmail.com>
2023-11-28 05:09:56 +0200

    +

--- js/task-3.js
+++ js/task-3.js
@@ -1,26 +1,15 @@
-const banWord1 = "spam";
-const banWord2 = "sale";
-
-const message1 = "Latest technology news"; // false
-const message2 = "JavaScript weekly newsletter"; // false
-const message3 = "Get best sale offers now!"; // true
-const message4 = "Amazing SalE, only tonight!"; // true
-const message5 = "Trust me, this is not a spam message"; // true
-const message6 = "Get rid of sPaM emails. Our book in on sale!"; // true
-const message7 = "[SPAM] How to earn fast money?"; // true
-
 function checkForSpam(message) {
-  const hasSpam1 =
-    message.toLowerCase().includes(banWord1) ||
-    message.toLowerCase().includes(banWord2);
+  const Sentence = message.toLowerCase();
 
-  console.log(hasSpam1);
+  const hasSpam = Sentence.includes("spam") || Sentence.includes("sale");
+
+  return hasSpam;
 }
 
-checkForSpam(message1);
-checkForSpam(message2);
-checkForSpam(message3);
-checkForSpam(message4);
-checkForSpam(message5);
-checkForSpam(message6);
-checkForSpam(message7);
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
